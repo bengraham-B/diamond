1b0e3c10d8d10e6d6e25b3e1f61f4a0428323ae8
Ben Graham <59644205+bengraham-B@users.noreply.github.com>
2025-11-17 22:30:24 +0200

    Working on Category fecth

---
+++ src/client/app/pages/category/Table.jsx
@@ -0,0 +1,65 @@
+"use client"
+import React, {useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react'
+import "./Table.scss"
+export default function Table() {
+    const { data: session } = useSession()
+    
+
+    const [categories, setCategories] = useState();
+
+    //Y Fetch Categories
+    const fetchCategories = async() => {
+try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_ENV_SERVER_BASE}/api/category/get_user_categories`, {
+                method: "POST",
+                body: JSON.stringify({
+                    accountID: session.diamond.accountID
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            
+            if(response.ok){
+                const data = await response.json()
+                setCategories(data.categories)
+                console.log(data.categories)
+
+            } else {
+                notifyError("Could not Fetch Categories")
+            }
+
+        } catch (error) {
+            notifyError("Could not Fetch Categories", error)
+        }
+    }
+
+    useEffect(() => {
+        if(!session) return
+        fetchCategories()
+    },[session])
+
+    if (!session) return
+
+    return (
+        <main>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nr</th>
+                        <th>name</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {categories && categories.map((C, i) => (
+                        <tr key={C.id}>
+                            <td>{i+1}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </main>
+    )
+}
