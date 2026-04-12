7208b7e9f7371532e293ad84350f3a4b0c56d188
bengraham-B <grahamben7@gmail.com>
2026-04-12 12:26:14 +0200

    Added Theory Test to compare DB types in TABLE vs Data Types in C# Model.

--- src/DotNet/DiamondTest/UnitTest/DateTest.cs
+++ src/DotNet/DiamondTest/UnitTest/DateTest.cs
@@ -6,7 +6,9 @@ namespace DiamondTest.UnitTest;
 public class DateTest
 {
     [Theory]
+    [InlineData("2026-04-11", 11, 101, "Sat", 15, 4,  "Apr",  2026)]
     [InlineData("2026-04-12", 12, 102, "Sun", 15, 4,  "Apr",  2026)]
+    [InlineData("2026-04-13", 13, 103, "Mon", 16, 4,  "Apr",  2026)]
     public void DateBreakDownTest(string pDate, int pDay, int pDayOfYear, string pDayOfWeek, int pWeek, int pMonth, string pMonthName, int pYear)
     {
         DateBrokenDownModel mDate = DateUtils.BreakDownDate(pDate);
