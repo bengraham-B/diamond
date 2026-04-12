336a56969aa3f7b9e0dc3f7079da5b5f7c16e10f
bengraham-B <grahamben7@gmail.com>
2026-04-12 10:00:19 +0200

    Removed unused files from testing

--- src/DotNet/DiamondTest/IntergationTest/DiamondTransactionRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace DiamondTest.IntergationTest;
-
-public class DiamondTransactionRepository
-{
-    private readonly AppDbContext _context;
-    public DiamondTransactionRepository(AppDbContext context) => _context = context;
-
-    // public List<Class.DiamondTransaction> GetUserTXN() => _context.DiamondTransactions.ToList();
-}
