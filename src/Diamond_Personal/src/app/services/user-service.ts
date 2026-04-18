f947393a7f5e643253c32b3d8b81a4184552d189
bengraham-B <grahamben7@gmail.com>
2026-04-19 00:00:42 +0200

    Added DiamondResponse to Merchant

--- src/Diamond_Personal/src/app/services/user-service.ts
+++ src/Diamond_Personal/src/app/services/user-service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-	// accountID:string  = "de58d6a9-1512-11f1-a3e0-ce6cdc3544e3" // Main Account
-  accountID: string = "32acc5e7-14c7-11f1-a3e0-ce6cdc3544e3" // DEV_ACCOUNT
+	accountID:string  = "de58d6a9-1512-11f1-a3e0-ce6cdc3544e3" // Main Account
+  // accountID: string = "32acc5e7-14c7-11f1-a3e0-ce6cdc3544e3" // DEV_ACCOUNT
 }
--- src/DotNet/Merchant/MerchantCRUD.cs
+++ src/DotNet/Merchant/MerchantCRUD.cs
@@ -5,23 +5,40 @@ namespace Merchant;
 
 public class MerchantCRUD
 {
-    public void GetMerchants(Conn conn, RequestBody req)
+    public DiamondResponse GetMerchants(Conn conn, RequestBody req)
     {
         const string SQL = @"";
+        
+        return new DiamondResponse
+        {
+            Success = true
+        };
     }
     
-    public void AddMerchant(Conn conn, MerchantModel model)
+    public DiamondResponse AddMerchant(Conn conn, MerchantModel model)
     {
         const string SQL = @"";
+        return new DiamondResponse
+        {
+            Success = true
+        };
     }
     
-    public void UpdateMerchants(Conn conn, MerchantModel model)
+    public DiamondResponse UpdateMerchants(Conn conn, MerchantModel model)
     {
         const string SQL = @"";
+        return new DiamondResponse
+        {
+            Success = true
+        };
     }
     
-    public void DeleteMerchants(Conn conn, RequestBody req)
+    public DiamondResponse DeleteMerchants(Conn conn, RequestBody req)
     {
         const string SQL = @"DELETE FROM MERCHANT WHERE MERCHANT_ID=@MERCHANT_ID AND ACCOUNT_ID=@ACCOUNT_ID";
+        return new DiamondResponse
+        {
+            Success = true
+        };
     }
 }
